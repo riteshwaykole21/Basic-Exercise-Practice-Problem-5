fd1c61f3cc67cceb5e67c3874ad81eb2c752e7f8
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-05-08 09:54:23 +0530

    [Ritesh]add : Number Of Char In String

---
+++ BasicExercise/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercise
+{
+    public class Solution
+    {
+        public void Number()
+        {
+            Console.WriteLine("Enter A One String");
+            string Name = Console.ReadLine();
+            int Num = 0;    
+            foreach(char iteam in Name)
+            {
+                Num++;  
+            }
+            Console.WriteLine(Num); 
+
+        }
+        
+    }
+}
